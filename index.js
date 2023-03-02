047f054b48f14aa2f710e5861bb1aad221fbc247
suraj <mesurajsingh2000@gmail.com>
2023-03-02 19:27:29 +0530

    this is first commit only home

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.listen(port , function(err){
+if(err)
+{
+console.log(`error is running : ${err}`);
+}else
+{
+    console.log(`server up and running on port : ${port}`);
+}
+})  
+
+app.get('/' , function(req , res){
+
+    return res.sendFile(__dirname + "/home.html");
+    // res.send("<h1>hello</h1>")
+})
